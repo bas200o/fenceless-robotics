cc35ff5e4153075b0256b6a0e39999610aaaeee0
bas <bas@shitposts.nl>
2021-11-24 16:00:05 +0100

    [add] Linux project setup

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bas/Documents/git/fenceless-robotics
+# Build directory: /home/bas/Documents/git/fenceless-robotics/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
