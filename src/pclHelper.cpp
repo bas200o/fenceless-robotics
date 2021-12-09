07e7c57e879c1db8d9091f70ac0fc6f0ec28e6f5
Lucas Prins <lucas.snirp@gmail.com>
2021-12-09 15:13:28 +0100

    [Add] Method to add 2 pointclouds together

--- src/pclHelper.cpp
+++ src/pclHelper.cpp
@@ -20,7 +20,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr pclHelper::loadPCL(std::string file_path) {
 pcl::PointCloud<pcl::PointXYZ>::Ptr pclHelper::generatePCL() {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
   cloud->width=10;
-  cloud->height=2;
+  cloud->height=1;
   cloud->is_dense = false;
   cloud->points.resize(cloud->width * cloud->height);
 
@@ -31,4 +31,30 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr pclHelper::generatePCL() {
     cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
   }
   return cloud;
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr pclHelper::combinePointClouds(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2) {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr recieving_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+    recieving_cloud->width=cloud1->size()+cloud2->size();
+    recieving_cloud->height=1;
+    recieving_cloud->is_dense = true;
+    recieving_cloud->points.resize(recieving_cloud->width * recieving_cloud->height);
+
+    size_t cloud1Size = cloud1->size();
+    size_t cloud2Size = cloud2->size();
+
+    for (size_t  i = 0; i < cloud1Size; i++) {
+      recieving_cloud->points[i].x = cloud1->points[i].x;
+      recieving_cloud->points[i].y = cloud1->points[i].y;
+      recieving_cloud->points[i].z = cloud1->points[i].z;
+    }
+
+    for (size_t  i = 0; i < cloud2Size; i++) {
+      recieving_cloud->points[i + cloud1Size].x = cloud2->points[i].x;
+      recieving_cloud->points[i + cloud1Size].y = cloud2->points[i].y;
+      recieving_cloud->points[i + cloud1Size].z = cloud2->points[i].z;
+    }
+
+    return recieving_cloud;
 }
