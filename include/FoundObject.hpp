d8219c74c354068e0c1075422a3046bc65fc1ab5
InfraRoderik <roderik1999@hotmail.nl>
2021-12-23 12:31:42 +0100

    [Added] Object Detection
    Test class still in there as example

--- include/FoundObject.hpp
+++ include/FoundObject.hpp
@@ -1,15 +1,21 @@
 /*Objects found in 3d pcl*/
 #pragma once
 #include <tuple>
+#include <vector>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 class FoundObject
 {
 private:
     std::tuple<float, float> location;
     std::tuple<float, float> size;//misschien los width height depth
+    std::tuple<float, float> centerMass;
+    pcl::PointCloud<pcl::PointXYZRGB> objectCloud;
 
 public:
-    FoundObject(/* args */);
+    FoundObject(pcl::PointCloud<pcl::PointXYZRGB> objectCloud);
     ~FoundObject();
 };
 
