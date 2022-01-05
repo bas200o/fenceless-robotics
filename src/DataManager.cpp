195acdb27fe265459926fc416777aba602b28509
Yannick <ydvd1308@gmail.com>
2022-01-05 16:01:56 +0100

    UI Thread on main while seperate function in background

--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -4,19 +4,53 @@
 #include "../include/Controller2D.hpp"
 #include "../include/CameraConnector.hpp"
 
+// #include <tbb/tbb_thread.h>
+// #include <tbb/tbb.h>
+// #include <tbb/spin_mutex.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "oneapi/tbb/parallel_for.h"
+#include "oneapi/tbb/task_arena.h"
+// #include <tbb/parallel_for.h>
+
+int buildUI(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    GUIApplication gui;
+    gui.show();
+    return app.exec();
+}
+
+
 int main(int argc, char **argv)
 {
     CameraConnector *camCon = camCon->getInstance();
     camCon->connectCameras(2, 1);
 
+    tbb::task_arena arena;
+
+    arena.enqueue( [] {
+
+        CameraConnector *camCon = CameraConnector::getInstance();
+        camCon->connectCameras(2, 1);
+
+        Controller3D cont3;
+        Controller2D cont2;
+
+        cout << "Hello vision!" << endl;
+
+        // while(true)
+            //Do 3D vision
+
+    });
+    
+
     QApplication app(argc, argv);
     GUIApplication gui;
     gui.show();
-    app.exec();
 
-    Controller3D cont3;
-    Controller2D cont2;
-    //while(true)
-    std::cout << "Hello vision!" << endl;
-    //Do 3DVision
+    return app.exec();
 }
