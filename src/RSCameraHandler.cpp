f31484edd4868cdc5536ccaa37f1d48dbb962b91
Lucas-Ubuntu <lucas.snirp@gmail.com>
2022-01-14 10:06:45 +0100

    [fix] fixed merges

--- src/RSCameraHandler.cpp
+++ src/RSCameraHandler.cpp
@@ -280,7 +280,14 @@ void RSCameraHandler::connectCamera()
 {
 
     std::vector<std::string> serials;
- 
+    for (auto &&dev : ctx.query_devices())
+    {
+
+        serials.push_back(dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
+        // std::cout << dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER) << std::endl;
+    }
+
+
     std::vector<std::string> connectedCams = CameraConnector::getInstance()->getConnectedRSCameras();
     for (std::string serial : serials)
     {
